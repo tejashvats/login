d061371be4d47e1ebfb92ed052779d5cdb055fe5
“tejash <tejashvats2002@gmail.com>
2023-08-12 20:56:03 +0530

    adding function on user input area and password area

--- src/login.js
+++ src/login.js
@@ -1,23 +1,50 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { useState } from 'react';
 
-function login(){
-    const [user,setUser] = useState("");
-    const [password,setPassword] = useState("");
+function Login(){
+    const [user,setUser] = useState(0);
+    const [password,setPassword] = useState(0);
+    const [error,setError] = useState(false);
+    const [perror,setPerror] = useState(false);
    function loginhandel(e){
+    alert("login");
         e.preventDefault();
     }
+    function userHandler(e){
+        let item=e.target.value;
+        if(item.length<3) {
+            console.warn("username is too short");
+        setError(true);
+    }
+    else{
+        setError(false);
+    }
+}
+    function passwordHandler(e){
+        let item=e.target.value;
+        if(item.length<3) {
+            console.warn("username is too short");
+        setPerror(true);
+    }
+    else{
+        setPerror(false);
+    }
+       
+    }
+  
     return(
         <div>
             
             < h1> Login </h1>
             <form onSubmit={loginhandel}>
-            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Username" onChange={userHandler} />{error? <span>invalid </span> : null}
             <br /><br />
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" onChange={passwordHandler} />{perror? <span>invalid </span> : null}
             <br /><br />
             <button type='submit'> Login </button>
             </form> 
            </div>   
            )
+
 }
-export default login;
+export default Login;
